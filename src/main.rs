bc4834eff1fd01c96afa089dfd3ad1ddb93bcc04
superuser122 <vasileiosnl@gmail.com>
2022-01-16 13:21:27 +0200

    clean

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,6 @@ use std::env;
 use std::fs::OpenOptions;
 use std::error::Error;
 
-
-
 struct Handler;
 
 #[async_trait]
@@ -17,11 +15,10 @@ impl EventHandler for Handler {
     }
 
     async fn message(&self, ctx: Context, msg: Message) {
-
         if msg.content.contains("!bug") {
             let response = match write_to_file(&msg){
                 Ok(_) => String::from("Bug report was saved"),
-                Err(e) => format!("Error saving bug report{}", e.to_string())
+                Err(e) => format!("Error saving bug report: {}", e.to_string())
             };
 
             let r = msg.channel_id.send_message(&ctx.http, |m| {
@@ -39,9 +36,9 @@ fn write_to_file(msg: &Message) -> Result<(), Box<dyn Error + Send + Sync>>{
     let content = msg.content.replace("!bug ", "");
     let text =format!("From: {}\nDate: {}\nMessage:{}\n\n", msg.author.name, msg.timestamp.to_string(), content);
     let mut file = OpenOptions::new()
-            .append(true)
-            .create(true)
-            .open(path)?;
+        .append(true)
+        .create(true)
+        .open(path)?;
     file.write_all(text.as_bytes())?;
     Ok(())
 }
